161bc3680d7360f2f17eda9e1d2f95a925077496
Imonisa Brian <mcbriandavids43@gmail.com>
2022-04-02 13:01:25 +0100

    April 4/2/22

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const App = ()=>{
+  return(
+    <div> <h2> Welcome {new Date().toString()}</h2> </div>
+  )
+}
+export default App
---
+++ src/index.js
@@ -0,0 +1,6 @@
+require("file-loader?name=[name].[ext]!./index.html");
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+ReactDom.render(<App />, document.getElementById("root"));
